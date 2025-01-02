6a201b15fabc2051cfec732efa0a227c3316b53c
daikonk <91625729+daikonk@users.noreply.github.com>
2025-01-02 15:33:16 -0800

    fixed shiftwidth and added gruvbox theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
-vim.oshiftwidth = 4
+vim.o.shiftwidth = 4
 
 require("lazy").setup({
 	spec = {
@@ -31,4 +31,4 @@ require("lazy").setup({
 	},
 })
 
--- vim: ts=2 sts=2 sw=2
+-- vim: ts=4 sts=4 sw=4
